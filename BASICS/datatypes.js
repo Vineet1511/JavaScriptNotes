24c53a276f21d2ea9910813c28fb538c8c4dec20
Vineet1511 <142674189+Vineet1511@users.noreply.github.com>
2024-01-16 07:20:50 +0000

    // Datatypes in JS

---
+++ BASICS/datatypes.js
@@ -0,0 +1,20 @@
+// Datatypes in JS
+
+Number
+BigInt
+String // used in double quotes and single quotes and back ticks
+Boolean
+null // standalone value not 0 and datatype is Object
+undefined // when not assigned any value
+Symbol
+
+//Special data type
+
+Object
+
+let boolValue = "33abc";
+let convertNum = Number (boolValue)
+
+console.log(convertNum);
+console.log(typeof convertNum);
+
